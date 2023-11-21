064ec5723d1fa97249ac2b931fd365742c1193ae
Duo Xu <duoxu@Duos-MacBook-Pro-2.local>
2023-11-20 22:01:29 -0800

    make pagination

--- lib/news_list.dart
+++ lib/news_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'news_card.dart';
 
 class NewsList extends StatefulWidget {
@@ -9,38 +8,74 @@ class NewsList extends StatefulWidget {
 }
 
 class _NewsListState extends State<NewsList> {
+  late ScrollController _scrollController;
+  late List<DocumentSnapshot> _articles;
+  bool _isLoading = false;
+  final _pageSize = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    _articles = [];
+    _fetchNews();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Atlas News App'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('news').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          }
-
-          var articles = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: (context, index) {
-              var articleData = articles[index].data()! as Map<String, dynamic>;
-              return NewsCard(articleData: articleData);
-            },
-          );
-        },
-      ),
+      body: _buildNewsList(),
     );
+  }
+
+  Widget _buildNewsList() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _articles.length,
+      itemBuilder: (context, index) {
+        var articleData = _articles[index].data() as Map<String, dynamic>;
+        return NewsCard(articleData: articleData);
+      },
+    );
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      _fetchNews();
+    }
+  }
+
+  void _fetchNews() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      QuerySnapshot<Object?> querySnapshot;
+
+      if (_articles.isNotEmpty) {
+        querySnapshot = await FirebaseFirestore.instance
+            .collection('news')
+            .orderBy('title')
+            .startAfterDocument(_articles.last)
+            .limit(_pageSize)
+            .get();
+      } else {
+        querySnapshot = await FirebaseFirestore.instance
+            .collection('news')
+            .orderBy('title')
+            .limit(_pageSize)
+            .get();
+      }
+
+      setState(() {
+        _isLoading = false;
+        _articles.addAll(querySnapshot.docs);
+      });
+    }
   }
 }
